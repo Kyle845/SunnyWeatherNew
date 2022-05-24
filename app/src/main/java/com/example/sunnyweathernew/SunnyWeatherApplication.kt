eafcbacece0710d60e9c1e954ce1e14b7574d85c
lixingyu06 <lixingyu06@kuaishou.com>
2022-05-24 16:10:28 +0800

    基础架构搭建

---
+++ app/src/main/java/com/example/sunnyweathernew/SunnyWeatherApplication.kt
@@ -0,0 +1,18 @@
+package com.example.sunnyweathernew
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application(){
+  companion object {
+    const val TOKEN = "nqygJBOgFVVSVjLv"
+    @SuppressLint("StaticFieldLeak")
+    lateinit var context : Context
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    context = applicationContext
+  }
+}
